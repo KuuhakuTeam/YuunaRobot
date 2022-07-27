df2e83abd178a1d0e851ab7e747d18c367ca4a79
fnixdev <luisgatn000@gmail.com>
2022-07-26 23:01:31 -0300

    +

--- yuuna/plugins/start.py
+++ yuuna/plugins/start.py
@@ -63,7 +63,7 @@ async def start_(c: yuuna, m: Union[Message, CallbackQuery]):
 
 
 @yuuna.on_callback_query(filters.regex(pattern=r"^infos$"))
-async def infos(c: yuuna):
+async def infos(c: yuuna, cq: CallbackQuery):
     info_text = f"""
 **♬ Bot Info ♪**
 
